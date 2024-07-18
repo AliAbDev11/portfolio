0f809c6b6d5f52ba5d5ec3ff9294941cf0011ebc
Ali AB <aliabdev07@gmail.com>
2024-07-18 15:56:57 +0100

    Update routes.py
    add save pic def
    update def profile

--- website/routes.py
+++ website/routes.py
@@ -2,7 +2,7 @@ import secrets
 from PIL import Image
 import os
 from website.models import User, Profile, db, Education, Experience, Service, Skill, Project, Contact
-from flask import render_template, url_for, flash, redirect, request
+from flask import current_app, render_template, url_for, flash, redirect, request
 from website.forms import RegistrationForm, LoginForm, UpdateProfileForm
 from website import app, bcrypt
 from sqlalchemy.orm import joinedload
@@ -13,6 +13,9 @@ from flask_login import (
     logout_user,
     login_required,
 )
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 profiles = [
@@ -41,11 +44,19 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_name = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/user_pics", picture_name)
+    picture_dir = os.path.join(current_app.root_path, 'static', 'images/user_pics')
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(picture_dir):
+        os.makedirs(picture_dir)
+
+    picture_path = os.path.join(picture_dir, picture_name)
+
     output_size = (150, 150)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+    
     return picture_name
 
 # Custom error handler for 404 errors
@@ -127,62 +138,48 @@ def logout():
 def profile():
     profile_form = UpdateProfileForm()
 
-    # Fetch current_user from Flask-Login
-    user_id = current_user.id
-    current_user_obj = User.query.options(joinedload(User.profile)).filter_by(id=user_id).first()
-
     if profile_form.validate_on_submit():
-        # Handle form submission and update database
-        if not current_user_obj.profile:
-            # If profile doesn't exist, create a new one
-            new_profile = Profile(
-                user_id=user_id,
-                phone_number=profile_form.phone_number.data,
-                bio_title=profile_form.bio_title.data,
-                bio=profile_form.bio.data,
-                github=profile_form.github.data,
-                linkedin=profile_form.linkedin.data,
-                twitter=profile_form.twitter.data,
-                instagram=profile_form.instagram.data
-            )
-            db.session.add(new_profile)
-        else:
-            # If profile exists, update it
-            current_user_obj.profile.phone_number = profile_form.phone_number.data
-            current_user_obj.profile.bio_title = profile_form.bio_title.data
-            current_user_obj.profile.bio = profile_form.bio.data
-            current_user_obj.profile.github = profile_form.github.data
-            current_user_obj.profile.linkedin = profile_form.linkedin.data
-            current_user_obj.profile.twitter = profile_form.twitter.data
-            current_user_obj.profile.instagram = profile_form.instagram.data
+        # Handle profile picture upload
+        if profile_form.picture.data:
+            picture_file = save_picture(profile_form.picture.data)
+            current_user.profile.image_file = picture_file
 
-        db.session.commit()
-        flash("Your profile has been updated", "success")
-        return redirect(url_for("profile"))
+        # Update user and profile data
+        current_user.fullname = profile_form.fullname.data
+        current_user.username = profile_form.username.data
+        current_user.email = profile_form.email.data
+        current_user.profile.phone_number = profile_form.phone_number.data
+        current_user.profile.address = profile_form.address.data
+        current_user.profile.bio_title = profile_form.bio_title.data
+        current_user.profile.bio = profile_form.bio.data
+        current_user.profile.github = profile_form.github.data
+        current_user.profile.linkedin = profile_form.linkedin.data
+        current_user.profile.twitter = profile_form.twitter.data
+        current_user.profile.instagram = profile_form.instagram.data
 
-    elif request.method == "GET":
-        # Populate form fields with data from models
-        if current_user_obj:
-            profile_form.fullname.data = current_user_obj.fullname
-            profile_form.email.data = current_user_obj.email
-            profile_form.username.data = current_user_obj.username
+        try:
+            db.session.commit()
+            flash('Profile updated successfully', 'success')
+            return redirect(url_for('profile'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'An error occurred while updating your profile: {str(e)}', 'danger')
 
-            if current_user_obj.profile:
-                profile_form.phone_number.data = current_user_obj.profile.phone_number
-                profile_form.bio_title.data = current_user_obj.profile.bio_title
-                profile_form.bio.data = current_user_obj.profile.bio
-                profile_form.github.data = current_user_obj.profile.github
-                profile_form.linkedin.data = current_user_obj.profile.linkedin
-                profile_form.twitter.data = current_user_obj.profile.twitter
-                profile_form.instagram.data = current_user_obj.profile.instagram
+    elif request.method == 'GET':
+        # Populate form fields with current user data
+        profile_form.fullname.data = current_user.fullname
+        profile_form.username.data = current_user.username
+        profile_form.email.data = current_user.email
+        profile_form.phone_number.data = current_user.profile.phone_number
+        profile_form.address.data = current_user.profile.address
+        profile_form.bio_title.data = current_user.profile.bio_title
+        profile_form.bio.data = current_user.profile.bio
+        profile_form.github.data = current_user.profile.github
+        profile_form.linkedin.data = current_user.profile.linkedin
+        profile_form.twitter.data = current_user.profile.twitter
+        profile_form.instagram.data = current_user.profile.instagram
 
     # Determine the image file path for rendering
-    image_file = url_for("static", filename=f"images/user_pics/{current_user_obj.profile.image_file}") if current_user_obj.profile and current_user_obj.profile.image_file else url_for("static", filename="images/user_pics/default.png")
+    image_file = url_for("static", filename=f"images/user_pics/{current_user.profile.image_file}") if current_user.profile and current_user.profile.image_file else url_for("static", filename="images/user_pics/default.png")
 
-    return render_template(
-        "admin/profile.html",
-        title="Profile",
-        profile_form=profile_form,
-        image_file=image_file,
-        active_tab="profile"
-    )
+    return render_template('admin/profile.html', title='Profile', image_file=image_file, profile_form=profile_form, active_tab="profile")
