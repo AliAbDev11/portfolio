edc83d0fa9acd2307fc042e017cb25d39db87e95
Ali AB <aliabdev07@gmail.com>
2024-07-22 15:14:36 +0100

    Update __init__.py
    Add config mail

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "62913a7dac3933f87a84626fcdeaaf9e2653f0a000843efd9bf2b31ba4767402"
@@ -15,6 +17,12 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+mail = Mail(app)
 
 from website.route_sections.experience import experience_bp
 from website.route_sections.education import education_bp
